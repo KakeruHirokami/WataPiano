d41d716d63f0f84e95e18141d71c18215d5e4edc
kakeruhirokami <kakeru.hirokami.stonerider@gmail.com>
2024-08-03 14:48:56 +0900

    Initial Commit

---
+++ WataPiano/WataPianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WataPianoApp.swift
+//  WataPiano
+//
+//  Created by 広上駆 on 2024/08/03.
+//
+
+import SwiftUI
+
+@main
+struct WataPianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
